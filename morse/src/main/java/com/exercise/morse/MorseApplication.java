ac2942ff119c522221dde2bfea410e96883e8e8d
carrilloign <ignacio.carrillo@mercadolibre.com>
2021-03-15 09:10:45 -0300

    reverting changes

--- morse/src/main/java/com/exercise/morse/MorseApplication.java
+++ morse/src/main/java/com/exercise/morse/MorseApplication.java
@@ -1,4 +1,5 @@
 package com.exercise.morse;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- morse/src/main/java/com/exercise/morse/MorseController.java
+++ morse/src/main/java/com/exercise/morse/MorseController.java
@@ -1,17 +1,11 @@
-package com.exercise.morse.controller;
+package com.exercise.morse;
 
-import com.exercise.morse.MorseApplication;
-import com.exercise.morse.service.MorseToSp;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MorseController {
-
-    @Autowired
-    private MorseToSp morseTranslator;
 
     @GetMapping
     public String sayHello(){
@@ -20,6 +14,6 @@ public class MorseController {
 
     @GetMapping(path = "/{morse}")
     public String morseToSp(@PathVariable String morse){
-        return  morseTranslator.toSp(morse);
+        return  MorseToSp.toSp(morse);
     }
 }
