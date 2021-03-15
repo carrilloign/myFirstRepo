a889a8c47d932af1ca466cdf3ae1967801724c63
carrilloign <ignacio.carrillo@mercadolibre.com>
2021-03-15 09:12:17 -0300

    translate to english, and implement mvc. Ready to review

--- morse/src/main/java/com/exercise/morse/MorseApplication.java
+++ morse/src/main/java/com/exercise/morse/MorseApplication.java
@@ -1,5 +1,4 @@
 package com.exercise.morse;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- morse/src/main/java/com/exercise/morse/controller/MorseController.java
+++ morse/src/main/java/com/exercise/morse/controller/MorseController.java
@@ -1,11 +1,17 @@
-package com.exercise.morse;
+package com.exercise.morse.controller;
 
+import com.exercise.morse.MorseApplication;
+import com.exercise.morse.service.MorseToSp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MorseController {
+
+    @Autowired
+    private MorseToSp morseTranslator;
 
     @GetMapping
     public String sayHello(){
@@ -14,6 +20,6 @@ public class MorseController {
 
     @GetMapping(path = "/{morse}")
     public String morseToSp(@PathVariable String morse){
-        return  MorseToSp.toSp(morse);
+        return  morseTranslator.toSp(morse);
     }
 }
